ba190f136ae99a173ced36507a2b25c20e18b115
Sarah Hoffmann <lonvia@denofr.de>
2021-01-31 18:34:17 +0100

    avoid direct references to node_modules
    Rollup will infer the correct directory for external
    resources.

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import * as jquery from 'jquery';
-  import '../node_modules/bootstrap/dist/css/bootstrap.css';
-  import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+  import 'bootstrap/dist/css/bootstrap.css';
+  import 'bootstrap/dist/js/bootstrap.bundle.js';
 
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
