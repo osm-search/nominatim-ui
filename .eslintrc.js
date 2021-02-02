aa6c7ac0c7535db250027e164299d1a6a45222b1
marc tobias <mtmail@gmx.net>
2021-02-02 22:49:56 +0100

    Add eslint and configuration. it reports 114 issues

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    'airbnb-base/legacy'
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    jquery: true
+  },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['*'],
+      globals: {
+        L: true // leaflet library
+      },
+      rules: {
+        camelcase: 'off', // my_var is fine, no need for myVar
+        'func-names': 'off', // anonymous 'function()' is fine
+        'vars-on-top': 'off',
+        'new-cap': 'off', // constructor name can start lowercase (as Leaflet does)
+        'no-multiple-empty-lines': 'off',
+        'padded-blocks': 'off',
+        'no-param-reassign': 'off',
+        'max-len': [
+          'error',
+          100,
+          2,
+          {
+            ignoreUrls: true,
+            ignoreComments: false
+          }
+        ]
+      }
+    },
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ]
+};
