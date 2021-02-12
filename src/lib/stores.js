7e3e971ca032ae02e36f794225376a59a646f273
Sarah Hoffmann <lonvia@denofr.de>
2021-02-12 17:40:41 +0100

    add documentation for refresh_page() function

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -5,6 +5,16 @@ export const results_store = writable();
 export const last_api_request_url_store = writable();
 export const page = writable();
 
+/**
+ * Update the global page state.
+ *
+ * When called without a parameter, then the current window.location is
+ * parsed and the page state is set accordingly. Otherwise the page state
+ * is set from the parameters. 'pagename' is the overall subpage (without
+ * .html extension). 'params' must be an URLSearchParams object and contain
+ * the requested query parameters. It may also be omitted completely for a
+ * link without query parameters.
+ */
 export function refresh_page(pagename, params) {
   if (typeof pagename === 'undefined') {
     pagename = window.location.pathname.replace('.html', '').replace(/^.*\//, '');
