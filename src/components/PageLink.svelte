21bcfb32a13839ff8c69a452d09e294da800a15a
mtmail <mtmail@gmx.net>
2021-02-18 16:31:34 +0100

    details page: add -search by name- for address lines having no osm_id (#76)

--- src/components/PageLink.svelte
+++ src/components/PageLink.svelte
@@ -1,12 +1,19 @@
 <script>
-import { refresh_page } from '../lib/stores.js';
+  import { refresh_page } from '../lib/stores.js';
 
-export let page;
-export let extra_classes = '';
+  export let page;
+  export let params_hash = {};
+  export let extra_classes = '';
+  let href = page + '.html';
 
-function handleClick() {
-  refresh_page(page);
-}
+  function handleClick() {
+    refresh_page(page, new URLSearchParams(params_hash));
+  }
+
+  $: {
+    let param_str = new URLSearchParams(params_hash).toString();
+    href = page + '.html' + (param_str ? '?' : '') + param_str;
+  }
 </script>
 
-<a on:click|preventDefault|stopPropagation={handleClick} href="{page}.html" class={extra_classes}><slot></slot></a>
+<a on:click|preventDefault|stopPropagation={handleClick} href={href} class={extra_classes}><slot></slot></a>
