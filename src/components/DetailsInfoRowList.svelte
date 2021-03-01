f2633dcc2939466943d2257439c12775fb712a11
Sarah Hoffmann <lonvia@denofr.de>
2021-03-01 20:42:17 +0100

    Add component for rows in details info box (#96)
    * add components for rows in details info box
    * bring back compact list style in details

---
+++ src/components/DetailsInfoRowList.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let items;
+</script>
+
+<style>
+  .name{
+    font-weight: bold;
+  }
+</style>
+
+{#each Object.keys(items) as name}
+  <div class="line">
+    <span class="name">{items[name]}</span> ({name})
+  </div>
+{/each}
