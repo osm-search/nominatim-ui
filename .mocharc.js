b843ed7fe8a3cfe676f61e4cbf211d4a2a95d8a6
marc tobias <mtmail@gmx.net>
2021-04-19 14:40:33 +0200

    Basic test suite with headless browser

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+// https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.js
+
+'use strict';
+
+module.exports = {
+  ignore: ['test/_bootstrap.js'],
+  require: ['test/_bootstrap.js'],
+  timeout: '5s'
+};
