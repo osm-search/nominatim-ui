d1075b7512f5004614ac98c174f2ba699cb510b9
Marc Tobias <mtmail@gmx.net>
2022-11-16 12:42:18 +0100

    convert mocha and eslint configuration to json file

--- .mocharc.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.js
-
-'use strict';
-
-module.exports = {
-  ignore: ['test/_bootstrap.js'],
-  require: ['test/_bootstrap.js'],
-  timeout: '5s'
-};
