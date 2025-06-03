3f27d285d124f3f50568a1f7891c01e117e2cf2d
mtmail <mtmail@gmx.net>
2025-06-03 23:26:14 +0200

    npm package updates (#283)

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import { spawn } from 'child_process';
 import css from 'rollup-plugin-css-only';
 import { readFileSync, writeFileSync } from 'fs';
