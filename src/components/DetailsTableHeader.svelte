fabc5d977909aed519ed11a281bba720abba28a3
mtmail <mtmail@gmx.net>
2025-09-10 01:10:26 +0200

    details page: HTML cleanup, use more tables (#304)

---
+++ src/components/DetailsTableHeader.svelte
@@ -0,0 +1,11 @@
+<thead>
+  <tr>
+    <th>Local name</th>
+    <th>Type</th>
+    <th>OSM</th>
+    <th>Address rank</th>
+    <th>Admin level</th>
+    <th>Distance</th>
+    <th></th>
+  </tr>
+</thead>
