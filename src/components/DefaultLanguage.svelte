dfd74ba17afc86303710aac9749f0c150dd7eaeb
mtmail <mtmail@gmx.net>
2025-11-24 23:27:18 +0100

    add note about browser default language setting (#315)

---
+++ src/components/DefaultLanguage.svelte
@@ -0,0 +1,3 @@
+<small>
+  Unless languages is set, your browser is using "<code>{navigator.language}</code>" (<a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Accept-Language">details</a>).
+</small>
