10cd92c1d05ab033c770d46b430e009508a2c314
mtmail <mtmail@gmx.net>
2025-12-12 22:36:46 +0100

    better escaping when building wikipedia url (#323)

--- src/components/WikipediaLink.svelte
+++ src/components/WikipediaLink.svelte
@@ -1,11 +1,13 @@
 <script>
-  import escapeHtml from 'escape-html';
-
+  // de:Überwasserkirche
   let { wikipedia } = $props();
 
   const linkParts = $derived(wikipedia ? wikipedia.split(':', 2) : null);
+  const subDomain = $derived(String(linkParts[0]).toLowerCase().replace(/[^a-z0-9-]/g, ''));
+
+  // https://de.wikipedia.org/wiki/%C3%9Cberwasserkirche
   const href= $derived(
-    `https://${escapeHtml(linkParts[0])}.wikipedia.org/wiki/${escapeHtml(linkParts[1])}`);
+    `https://${subDomain || 'en'}.wikipedia.org/wiki/${encodeURIComponent(linkParts[1])}`);
 </script>
 
 {#if wikipedia}
